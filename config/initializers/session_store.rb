8fc0a3e605f9f2f45243266a9d35ce151daa180c
Rémy Hannequin <remy.hannequin@gmail.com>
2015-02-17 23:12:50 +0100

    Init project from funky_starter

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_funkystarter_session'
+Rails.application.config.session_store :cookie_store, key: '_timeline_session'
