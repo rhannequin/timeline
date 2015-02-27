e33346c651c435156f4d3bd9bd70c48f295425bc
Rémy Hannequin <remy.hannequin@gmail.com>
2015-02-27 12:00:00 +0100

    Omniauth callbacks refactor

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,12 +1,15 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    @user = User.from_omniauth(omniauth_auth)
-    sign_in_and_redirect @user
+  def self.provides_callback_for(provider)
+    class_eval %(
+      def #{provider}
+        @user = User.from_omniauth omniauth_auth
+        sign_in_and_redirect @user
+      end
+    )
   end
 
-  def twitter
-    @user = User.from_omniauth(omniauth_auth)
-    sign_in_and_redirect @user
+  %i(twitter facebook).each do |provider|
+    provides_callback_for provider
   end
 
   private
